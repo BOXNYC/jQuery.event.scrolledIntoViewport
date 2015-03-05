9d95e75b964457575438788dc0f4a1f81f57dd2a
Joseph James Weitzel <joe@box.biz>
2015-03-05 16:27:37 -0500

    Create jQuery.event.scrolledIntoViewport-0.1.js

---
+++ jQuery.event.scrolledIntoViewport-0.1.js
@@ -0,0 +1,27 @@
+$.event.special.scrolledIntoViewport = {
+  setup: function(options) {
+    options = $.extend({
+      scrollPane: document
+    }, options);
+    var $items = $(this),
+      $scrollPane = $(options.scrollPane);
+    $items.data('scrolledIntoViewport', {
+	    scrollPane: options.scrollPane
+    });
+    $scrollPane.on('scroll.scrolledIntoViewport', function(){
+      $items.each(function(){
+        var $item = $(this);
+        $item.trigger('scrolledIntoViewport', {
+	        top: $item.offset().top - $scrollPane.scrollTop()
+        });
+      });
+    });
+    return false;
+  },
+  teardown: function(options) {
+    console.log(this, options, $(this).data('scrolledIntoViewport'));
+    var data = $(this).data('scrolledIntoViewport');
+    $(data.scrollPane).off('scroll.scrolledIntoViewport');
+    return false;
+  }
+};
